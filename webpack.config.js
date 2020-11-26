a76120d10c12dd9ae8a907a844ab25ce3da954a8
dlehdanakf <dlehdanakf@konkuk.ac.kr>
2020-11-26 22:50:15 +0900

    chore: Project initialize / 타입스크립트 및 웹팩과 로더 관련 라이브러리들 설치

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const MiniCssExtractPlugin = require(`mini-css-extract-plugin`);
+const path = require(`path`);
+const fs = require(`fs`);
+
+const srcPath = path.resolve(__dirname, `./src/`);
+const distPath = path.resolve(__dirname, `./dist/`);
+
+module.exports = (mode = `development`) => {
+	return {
+		mode,
+		resolve: {
+			extensions: [`.ts`, `.scss`],
+			alias: {
+				"@": srcPath
+			}
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					exclude: /node_modules/,
+					use: [ `ts-loader` ]
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						{ loader: MiniCssExtractPlugin.loader },
+						{ loader: "css-loader" },
+						{ loader: "sass-loader", options: { sourceMap: true, sassOptions: { outputStyle: `compressed` }}}
+					]
+				}
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({ filename: '[name].css' })
+		],
+		entry: {
+			'script': `${srcPath}/scripts/script.ts`,
+			'style': `${srcPath}/styles/style.scss`
+		},
+		output: {
+			path: distPath,
+			filename: `[name].js`
+		}
+	};
+};
