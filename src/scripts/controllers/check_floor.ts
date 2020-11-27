1b65f6d7f7ddfea967d7700cdfa054b46828b29d
dlehdanakf <dlehdanakf@konkuk.ac.kr>
2020-11-27 13:07:07 +0900

    feat: n 층의 버튼 활성 여부를 판단하는 전역 API 메소드 내부 구현

--- src/scripts/controllers/check_floor.ts
+++ src/scripts/controllers/check_floor.ts
@@ -3,6 +3,14 @@ import Elevator from "../data_models/Elevator";
 
 export const generate_isFloorCallable = (floorList: Array<Floor>, elevatorList: Array<Elevator>): (floorIndex: number) => boolean => {
     return (floorIndex: number): boolean => {
-        return false;
+        const floor: Floor = floorList[floorIndex - 1];
+        if(floor === undefined) {
+            /** 주어진 floorIndex 번호가 배열에 맞지 않는 경우 */
+            return undefined;
+        }
+
+        /** 데이터 모델의 isElevatorComing 필드를 활용하면 손쉽게 활성 여부 판단 가능 */
+        /** 오고있는 엘리베이터가 없다면 호출 버튼이 활성상태일 것 이므로 */
+        return floor.isElevatorComing === false;
     };
 };
