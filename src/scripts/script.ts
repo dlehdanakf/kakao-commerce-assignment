ef28a596576d52ee60a312c051599dac62b6c998
dlehdanakf <dlehdanakf@konkuk.ac.kr>
2020-11-27 20:03:34 +0900

    feat: 층 수, 엘리베이터 수 입력하는 폼 구현, 초기화 버튼 누름 시 Floor, Elevator 객체 소멸

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -10,19 +10,61 @@ declare global {
     interface Window {
         elevator: ElevatorAPI
     }
+    interface Document {
+        variable_form: HTMLFormElement
+    }
+}
+interface VariableFormInterface {
+    floor: HTMLInputElement,
+    elevator: HTMLInputElement,
+    submit: HTMLButtonElement,
+    reset: HTMLButtonElement
 }
 
-const floor_count = 5, elevator_count = 4;
-document.addEventListener(`DOMContentLoaded`, () => {
-    const { floorList, elevatorList } = initializeApplicationModels(floor_count, elevator_count);
+const disableForm = (form: VariableFormInterface) => {
+    form.floor.disabled = form.elevator.disabled = form.submit.disabled = true;
+    form.reset.disabled = false;
+};
+const enableForm = (form: VariableFormInterface) => {
+    form.floor.disabled = form.elevator.disabled = form.submit.disabled = false;
+    form.reset.disabled = true;
+};
 
+const runApplication = (form: VariableFormInterface) => {
+    const { value: floorCount } = form.floor;
+    const { value: elevatorCount } = form.elevator;
+
+    const { floorList, elevatorList } = initializeApplicationModels(+floorCount, +elevatorCount);
     window.elevator = {
         callElevator: generate_callElevator(floorList, elevatorList),
         isFloorCallable: generate_isFloorCallable(floorList, elevatorList)
     };
 
-    setTimeout(() => {
+    document.variable_form.addEventListener(`reset`, function eventListener() {
         floorList.forEach(e => e.destruct());
         elevatorList.forEach(e => e.destruct());
-    }, 4000);
+
+        floorList.length = 0;
+        elevatorList.length = 0;
+        window.elevator = undefined;
+
+        enableForm(form);
+        document.variable_form.removeEventListener(`reset`, eventListener);
+    });
+};
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    document.variable_form.addEventListener(`submit`, e => {
+        e.preventDefault();
+
+        const form: VariableFormInterface = {
+            floor: document.querySelector(`#variable-form-floor`),
+            elevator: document.querySelector(`#variable-form-elevator`),
+            submit: document.querySelector(`#variable-form-submit`),
+            reset: document.querySelector(`#variable-form-reset`)
+        };
+
+        disableForm(form);
+        runApplication(form);
+    });
 });
